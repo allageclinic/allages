0547093d927d4b116bfc119988565c4661a5f553
wowamin <wowamin125@gmail.com>
2026-04-08 10:26:36 +0800

    DEBUG

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var swiper = new Swiper (".slide-content", {
         slidesPerView: 3,
-        spaceBetween: 20,
+        spaceBetween: 25,
         loop: true,
         centeredSlides: true,
         grabCursor: true,
-        autoHeight: false,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -15,23 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
         breakpoints: {
-            0: { 
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            576: { 
-                slidesPerView: 1.5,
-                spaceBetween: 15,
-            },
-            768: { 
-                slidesPerView: 2,
-                spaceBetween: 15,
-            },
-            1024: { 
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
+            0: { slidesPerView: 1, },
+            520: { slidesPerView: 2 },
+            950: { slidesPerView: 3 },
         },
     });
 });
